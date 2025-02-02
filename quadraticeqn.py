b8387c5c0ea02a81f554707c9e34cf18bede74f3
Mikegm19 <101859418+Mikegm19@users.noreply.github.com>
2025-02-02 23:53:35 +0100

    Add files via upload

---
+++ quadraticeqn.py
@@ -0,0 +1,10 @@
+x=int(input("Enter integer x"))
+a=int(input("Enter integer a"))
+b=int(input("Enter integer b"))
+c=int(input("Enter integer c"))
+w=(b**2)-(4*a*c)
+w=w**0.5
+y=2*a
+z=w/y
+print(f"Tge solution to your quadratic equation is {z} and {-z}")
+
